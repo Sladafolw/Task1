47ec235eb61ab7377351f5a205995c9cb9493425
Sladafolw <109578734+Sladafolw@users.noreply.github.com>
2023-08-01 01:19:38 +0300

    Created layout

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,7 @@ int a = 0;
 do
 {
 
-    Console.WriteLine(" input 1 if you want create 100 files,\n input 2 if you want merge files to one,\n input 3 if you want add data to sql,\n input 4 if you want calculate sum of all integers and median of fractional numbers");
+    Console.WriteLine(" input 1 if you want create 100 files,\n input 2 if you want merge files to one,\n input 3 if you want add data to sql,\n input 4 if you want calculate sum of all integers and median of fractional numbers,\n input 5 to Close");
     try
     {
         a = Convert.ToInt32(Console.ReadLine());
@@ -19,7 +19,21 @@ do
             WorkingWithFile.Create100();
             Console.WriteLine("successfully");
             break;
-
+        case 2:
+            WorkingWithFile.Create100();
+            Console.WriteLine("successfully");
+            break;
+        case 3:
+            WorkingWithFile.Create100();
+            Console.WriteLine("successfully");
+            break;
+        case 4:
+            WorkingWithFile.Create100();
+            Console.WriteLine("successfully");
+            break;
+        case 5:
+            Environment.Exit(1);
+            break;
         default:
             break;
     }
